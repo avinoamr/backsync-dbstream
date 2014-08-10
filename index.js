d5a5b0e914fc03eb8a4cb7cbae88db42a3b3b04a
Roi Avinoam <avinoamr@gmail.com>
2014-08-11 00:42:06 +0300

    Use the code from albums

---
+++ index.js
@@ -0,0 +1,68 @@
+var backsync = require( "backsync" );
+module.exports = function ( connection ) {
+    var Cursor = connection.Cursor;
+
+    var update = function ( model, options, callback ) {
+        var obj = ( model.toJSON ) ? model.toJSON() : model;
+        var cursor = new Cursor();
+        cursor.once( "error", callback );
+        cursor.once( "finish", function () {
+            callback( null, obj );
+        });
+
+        ( !options.remove ) 
+            ? cursor.write( obj ) 
+            : cursor.remove({ id: model.id });
+
+        cursor.end();
+    }
+
+    var read = function ( model, options, callback ) {
+        new Cursor()
+            .find({ id: model.id })
+            .limit( 1 )
+            .once( "error", callback )
+            .once( "data", function ( obj ) {
+                callback( null, obj );
+                callback = null;
+            })
+            .once( "end", function () {
+                if ( callback ) {
+                    callback( new backsync.NotFoundError( model.id ) );
+                }
+            });
+    }
+
+
+    return backsync()
+        .create( update )
+        .update( update )
+        .read( read )
+        .patch( function ( model, options, callback ) {
+            read( model, { silent: true }, function ( err, obj ) {
+                if ( err ) return callback( err );
+                obj = _.extend( {}, obj, options.attrs );
+                update( obj, {}, callback );
+            });
+        })
+        .search( function ( collection, options, callback ) {
+            options.data || ( options.data = {} );
+            var data = _.clone( options.data || {} );
+            delete data[ "$sort" ];
+            delete data[ "$skip" ];
+            delete data[ "$limit" ];
+
+            var results = [];
+            var cursor = new Cursor()
+                .find( data )
+                .sort( options.data.$sort || null )
+                .skip( options.data.$skip || 0 )
+                .limit( options.data.$limit || Infinity )
+                .once( "error", callback )
+                .once( "end", function() { callback( null, results ); })
+                .on( "data", function ( obj ) { results.push( obj ) } )
+        })
+        .delete( function ( model, options, callback ) {
+            update( model, _.extend({ remove: true }, options ), callback );
+        });
+}
